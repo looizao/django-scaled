d0310898249574c6186e4fe7d5567b07da61f06f
Luiz Souza <luiz.souza@vinta.com.br>
2022-10-20 21:04:27 -0300

    before devcontainer

--- src/category/entities.py
+++ src/category/entities.py
@@ -1,6 +1,18 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+# from __seedwork.domain.entities import Entity
+# from __seedwork.domain.exceptions import EntityValidationException
 
-
+#kw_only ->
+#frozen  ->
+#slots   ->
+@dataclass(kw_only=True, frozen=True, slots=True)  # init, repr, eq
 class Category:
 
-    def __init__(self, name) -> None:
-        self.name = name
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=datetime.now
+    )
